95225a482f2ba5b2360fc743fdbc7b196cb3de2c
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2018-06-06 11:18:20 -0400

    pep8

--- populate.py
+++ populate.py
@@ -11,8 +11,10 @@ namespaces = {
     'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'
 }
 
+
 def log_warning(warning):
     print "WARNING:", warning
+
 
 def get_attr(element, attr):
     index = '{%s}%s' % (namespaces['w'], attr)
@@ -20,9 +22,13 @@ def get_attr(element, attr):
         return None
     return element.attrib[index]
 
-# Text inputs are a series of w:r elements at the same level bounded by
-# w:bookmarkStart and w:bookmarkEnd with common IDs.
+
 def set_text_input(docxml, name, value):
+    """
+    Text inputs are a series of w:r elements at the same level bounded by
+
+    w:bookmarkStart and w:bookmarkEnd with common IDs.
+    """
     start_selector = './/w:bookmarkStart[@w:name="%s"]' % name
     bookmark_start = docxml.find(start_selector, namespaces)
     bookmark_id = get_attr(bookmark_start, 'id')
@@ -40,30 +46,41 @@ def set_text_input(docxml, name, value):
             text_input.text = value
             break
 
+
 def find_next_element_matching(
         docxml,
         start_selector,
         check_element_lamdba,
         failure_message='Could not find following node matching condition'):
     label_element = docxml.find(start_selector, namespaces)
-    if label_element == None:
+    if label_element is None:
         raise Exception('Could not find name element with val=%s' % name)
 
     current_element = label_element
-    while current_element != None and check_element_lamdba(current_element):
+    while (
+        current_element is not None and
+        check_element_lamdba(current_element)
+    ):
         current_element = current_element.getnext()
 
-    if current_element == None:
+    if current_element is None:
         raise Exception(failure_message)
 
     return current_element
 
 
-# Checkbox elements are w:checkBox element that are preceeded by a w:name with val
-# equal to the checkbox name
-#
-# isChecked depends on the presence of an <w:checked> field inside this checkBox element
-def check_checkbox(docxml, name, should_be_checked):
+def check_checkbox(
+    docxml,
+    name,
+    should_be_checked
+):
+    """
+    Checkbox elements are w:checkBox element that are preceeded by a w:name
+    with val equal to the checkbox name
+
+    isChecked depends on the presence of an <w:checked> field inside this
+    checkBox element
+    """
     start_selector = './/w:name[@w:val="%s"]' % name
     checkbox_tag_name = "{%s}checkBox" % namespaces['w']
 
@@ -71,36 +88,51 @@ def check_checkbox(docxml, name, should_be_checked):
         docxml,
         start_selector,
         lambda element: element.tag != checkbox_tag_name,
-        failure_message="Could not find w:checkBox following a w:name with val=%s" % name
+        failure_message=(
+            "Could not find w:checkBox following a w:name with val=%s" % name
         )
+    )
 
-    check_element = checkbox_element.find('w:checked', namespaces);
+    check_element = checkbox_element.find('w:checked', namespaces)
     if should_be_checked and check_element is None:
         new_check_element = ET.Element('{%s}checked' % namespaces['w'])
         checkbox_element.append(new_check_element)
     elif not should_be_checked and check_element is not None:
         check_element.getparent().remove(check_element)
 
+
 def levenshtein_distance_nounicode(a, b):
-    return levenshtein_distance(a.decode(encoding='UTF-8'), b.decode(encoding='UTF-8'))
+    return levenshtein_distance(
+        a.decode(encoding='UTF-8'),
+        b.decode(encoding='UTF-8')
+    )
+
 
 def get_best_option_index(options, requested):
-    best_option = min(options, key=lambda option: levenshtein_distance_nounicode(requested, option))
+    best_option = min(
+        options,
+        key=lambda option: levenshtein_distance_nounicode(requested, option)
+    )
+
     if levenshtein_distance_nounicode(requested, best_option) != 0:
         log_warning(
             "Using best match '%s' for requested option '%s'" % (
                 best_option,
                 requested,
-                )
+            )
         )
 
     return options.index(best_option)
 
-# Dropdown elements are a w:ddList element that are preceeded by a w:name with val
-# equal to the dropdown name
-#
-# the selection depends on the presence of a <w:result w:val="$INDEX"/> field inside this checkBox element
+
 def select_option(docxml, name, option_to_select):
+    """
+    Dropdown elements are a w:ddList element that are preceeded by a w:name
+    with val equal to the dropdown name
+
+    the selection depends on the presence of a <w:result w:val="$INDEX"/> field
+    inside this checkBox element
+    """
     start_selector = './/w:name[@w:val="%s"]' % name
     dropdownlist_element_name = "{%s}ddList" % namespaces['w']
     val_attribute_name = '{%s}val' % namespaces['w']
@@ -109,17 +141,19 @@ def select_option(docxml, name, option_to_select):
         docxml,
         start_selector,
         lambda element: element.tag != dropdownlist_element_name,
-        failure_message="Could not find w:ddList following a w:name with val=%s" % name
+        failure_message=(
+            "Could not find w:ddList following a w:name with val=%s" % name
         )
+    )
 
     dropdown_option_elements = dropdown_element.findall(
         '{%s}listEntry' % namespaces['w']
-        )
+    )
     dropdown_options = [
         dropdown_option_element.attrib[val_attribute_name]
         for dropdown_option_element
         in dropdown_option_elements
-        ]
+    ]
 
     index = get_best_option_index(dropdown_options, option_to_select)
     result_element = dropdown_element.find('{%s}result' % namespaces['w'])
@@ -128,12 +162,13 @@ def select_option(docxml, name, option_to_select):
         dropdown_element.append(result_element)
     result_element.attrib[val_attribute_name] = str(index)
 
+
 def update_zip_in_place(
         input_zipfile,
         output_zipfile_name,
         replaced_file_in_zip_name,
         new_file_data
-        ):
+):
     print "updating file", replaced_file_in_zip_name, 'in zipfile'
     print "writing updated file to", output_zipfile_name
     with zipfile.ZipFile(output_zipfile_name, 'w') as zout:
